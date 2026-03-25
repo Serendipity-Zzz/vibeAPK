65d7f22f13ef5ad9155281c4397d3dab703a6292
Serendipity <3224191687@qq.com>
2026-03-25 15:54:20 +0800

    修复伪按键问题方法二+回退

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+}
+
 android {
     namespace = "com.example.karaoke_app"
     compileSdk = flutter.compileSdkVersion
